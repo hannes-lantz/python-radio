484ac52577fa90f316b50c287c136ccdd16b2004
lethalblackberry <hannes.lantz.97@gmail.com>
2023-06-07 22:03:07 +0200

    added some more stations

--- myradio.py
+++ myradio.py
@@ -6,7 +6,11 @@ urls = {
     "2": {"name": "P2", "url": "https://http-live.sr.se/p2-mp3-128"},
     "3": {"name": "P3", "url": "https://http-live.sr.se/p3-mp3-128"},
     "4": {"name": "P4", "url": "https://http-live.sr.se/p4malmo-mp3-128"},
-    "5": {"name": "RIX FM", "url": "https://fm01-ice.stream.khz.se/fm01_mp3"}
+    "5": {"name": "RIX FM", "url": "https://fm01-ice.stream.khz.se/fm01_mp3"},
+    "6": {"name": "Bandit Rock", "url": "https://fm02-ice.stream.khz.se/fm02_mp3"},
+    "7": {"name": "P3 Din Gata", "url": "https://live-cdn.sr.se/pool3/dingata/dingata.isml/dingata-audio=192000.m3u8"},
+    "8": {"name": "Mix Megapol", "url": "http://tx-bauerse.sharp-stream.com/http_live.php?i=mixmegapol_instream_se_mp3"},
+    "9": {"name": "NRJ", "url": "http://tx-bauerse.sharp-stream.com/http_live.php?i=nrj_instreamtest_se_mp3"}
 }
 
 # Create VLC instance
@@ -25,8 +29,8 @@ def print_menu():
 
 # Function to play the selected URL
 def play_url(url):
-    print(colored('-------------------------------------------------', 'yellow'))
-    print(colored("Now playing: " f"{url}", 'green'))
+    print(colored('=================================================', 'green'))
+    print(colored("Now playing: " f"{url}", 'yellow'))
     media = instance.media_new(url)
     player.set_media(media)
     player.play()
@@ -34,7 +38,7 @@ def play_url(url):
 # Main program loop
 while True:
     print_menu()
-    selection = input("Enter the station number (1-4), or 'q' to quit: ")
+    selection = input("Enter the station number (1-9), or 'q' to quit: ")
 
     if selection == "q":
         break
